b8d461cc6dd31dc0b0393e6f37bfee1e324e1c95
leotis <mekdemcrossit@tutanota.com>
2021-04-24 06:06:02 -0400

    Initial Commit

---
+++ LearnMiTimeTables/LearnMiTimeTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMiTimeTablesApp.swift
+//  LearnMiTimeTables
+//
+//  Created by Leotis buchanan on 2021-04-24.
+//
+
+import SwiftUI
+
+@main
+struct LearnMiTimeTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
